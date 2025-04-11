5fc58babb6e4ca53bab28e228f74c787cf8a8ea8
Netter7 <carlosbertolin7@gmail.com>
2025-04-11 23:11:21 +0200

    url, tagline y title

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,12 +5,12 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'CardioNotion',
+  tagline: 'Transforma tu práctica clínica',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docucardionotion.netlify.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
